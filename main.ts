372c25bc9acc88458e5533d76bf2d8bf4f3a8bfb
增利.申 <51192693+shengzengli@users.noreply.github.com>
2020-11-18 15:14:08 +0800

    编辑桃心

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 basic.forever(function () {
     basic.showString("shen")
     basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
     basic.pause(1000)
     basic.clearScreen()
 })
